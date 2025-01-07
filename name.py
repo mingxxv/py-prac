5f7f60959f6b32b21513de2d8210bdc7e6f5ed10
Gordon Chia <mingxxv@gmail.com>
2025-01-07 10:50:15 +0800

    chapter 2 name.py

---
+++ name.py
@@ -0,0 +1,14 @@
+name = "Ada Lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print("Languages:\n\tPython\n\tC\n\tJavascript")
+favorite_language = 'python '
+print(favorite_language.rstrip())
+nostarch_url = 'https://nostarch.com'
+print(nostarch_url.removeprefix('https://'))
