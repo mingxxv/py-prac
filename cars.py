b283f2aec5aa04af9580f179484ce635d45144b3
Gordon Chia <mingxxv@gmail.com>
2025-01-14 16:52:39 +0800

    14 Jan 2025 - finished chapter 3, added cars.py

---
+++ cars.py
@@ -0,0 +1,10 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+# cars.sort()
+# print(cars)
+# cars.sort(reverse=True)
+# print(cars)
+print("\nHere is the sorted list:")
+print(sorted(cars))
+cars.reverse()
+print(cars)
+print(len(cars))
